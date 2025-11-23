2e95c795ec1adde2826598cf37e79f79b8b86211
Tarun Bansal <tarunpayup@outlook.com>
2025-11-23 21:52:21 +0530

    New update

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,11 +1,13 @@
-import { ThemeProvider, DefaultTheme } from "@react-navigation/native";
+import { DarkTheme, DefaultTheme, ThemeProvider } from "@react-navigation/native";
 import { Stack } from "expo-router";
 import { StatusBar } from "expo-status-bar";
 import React from 'react';
+import { useColorScheme } from "react-native";
 
 export default function RootLayout(){
+  const colorScheme = useColorScheme();
   return(
-    <ThemeProvider>
+    <ThemeProvider value={colorScheme==="dark" ? DarkTheme:DefaultTheme}>
       <Stack>
         <Stack.Screen name="index" options={{title:"Expenses"}}/>
         <Stack.Screen name="add" options={{title:"Add Expenses"}}/>
